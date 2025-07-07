1327eb200e00d43dd6cda45c787e8b8068422739
DMProductionn <goluzin.dima008@mail.ru>
2025-07-07 18:33:38 +0500

    fix

--- src/app/(main)/crovlya/[product]/page.tsx
+++ src/app/(main)/crovlya/[product]/page.tsx
@@ -1,5 +1,10 @@
 import { ProductProvider } from '@/components/crovlya/products/ProductContext';
 import { ProductClient } from './ProductClient';
+import { DATA } from '@/components/crovlya/products/data';
+
+export function generateStaticParams() {
+  return DATA.map((product) => ({ product: product.slug }));
+}
 
 export default function Page({ params }: { params: { product: string } }) {
   return (
